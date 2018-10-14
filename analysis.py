581835933f5d08c6c28d4691d92d4a6209770bcc
william Asai <williamyasai@gmail.com>
2018-10-13 22:03:10 -0400

    response based on length

--- analysis.py
+++ analysis.py
@@ -17,6 +17,40 @@ def analysis(json_data):
 	for p in json_people:
 		chat_members.append(p["name"])
 	
+	json_messages.reverse()
+	sender = json_messages[0]['sender_name']
+	length_of_message = 0
+	total_message =''
+	response = ''
+	data_justin_to_reda = {'ID':[],'message_length':[],'response':[]}
+	ID = 0
+	for message in json_messages:
+		if 'content' in message:
+			if(sender == 'Reda Ali' and "Justin Chen" == message['sender_name']):
+				data_justin_to_reda['ID'].append(ID)
+				data_justin_to_reda['message_length'].append(length_of_message)
+				data_justin_to_reda['response'].append(response)
+				ID += 1
+				length_of_message = 0
+				response = ''
+				sender = "Justin Chen"
+			if message['sender_name'] == "Reda Ali":
+				response += message['content']
+				sender = "Reda Ali"	
+			if message['sender_name'] == "Justin Chen":
+				length_of_message += len(message['content'])
+
+
+	dataframe = pandas.DataFrame(data = data_justin_to_reda)
+	while True:
+		justin_input = input("Justin Chen: ")
+		for index,message in dataframe.iterrows():
+			if(message['message_length'] == len(justin_input)):
+				print(message['response'])
+				break
+		if justin_input == "end":
+			break
+	
 def main():
 	filename = "redaali.json"
 	db = parse(filename)
---
+++ test.py
@@ -0,0 +1,6 @@
+
+mylist = [1,2,3]
+listiter = iter(mylist)
+print(*listiter)
+next(listiter)
+print(*listiter)
